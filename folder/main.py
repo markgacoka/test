cece22c98250ba1483def7b7b06857033b22b51b
Mark Gacoka Mbui <markgacoka@gmail.com>
2023-02-06 15:25:35 -0800

    Add factorial as main code

--- folder/main.py
+++ folder/main.py
@@ -1,11 +1,5 @@
-from util import ImportedClass
-
-def print_output(text):
-    print(text)
-
-if __name__ == '__main__':
-    full_name = 'Gacoka Mbui'
-    imported_class_instance = ImportedClass(full_name)
-    titled_name = imported_class_instance.return_titled_name()
-    print_output(titled_name)
-
+def factorial(n):
+    if n < 2:
+        return 1
+    else:
+        return n * factorial(n-1)
--- folder/util.py
+++
@@ -1,6 +0,0 @@
-class ImportedClass:
-    def __init__(self, name):
-        self.name = name
-
-    def return_titled_name(self):
-        return 'Dr. ' + self.name
